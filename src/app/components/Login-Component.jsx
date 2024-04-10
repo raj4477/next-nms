d66df3e885791d807caa7c4f7355b6e1ee7279fa
Ritik Raj <80347492+raj4477@users.noreply.github.com>
2024-04-11 02:34:26 +0530

    login page button fixed

--- src/app/components/Login-Component.jsx
+++ src/app/components/Login-Component.jsx
@@ -185,7 +185,7 @@ const Login_Component = () => {
                     </form>
                     <button
                       onClick={handleSubmit}
-                      className="w-full text-white bg-#2563eb hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      className="w-full text-white  bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     >
                       Sign in
                     </button>
